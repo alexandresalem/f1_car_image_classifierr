7ec7bdc3352dab6f72dbb3564d6e4c0a6a26f8bf
Alexandre Salem <alexandresalem@salem-debian.domain-debian>
2020-05-16 00:06:44 -0500

    Download Photos v1 Ready

--- download_images/list_chassis.py
+++ download_images/list_chassis.py
@@ -1,16 +1,15 @@
 import os
-
+import json
+import re
+import time
 import requests
+import urllib.request as ulib
 from bs4 import BeautifulSoup
+from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-path = '/usr/local/bin/chromedriver'
-WINDOW_SIZE = "1920,1080"
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
-chrome_options.add_argument('--no-sandbox')
-# driver = webdriver.Chrome(executable_path=path)
+
+json_file = 'chassis_season.json'
 
 # This function searchs Wikipedia for the information of the teams of each year.
 # Then creates a file containing a dictionary with the results
@@ -38,11 +37,88 @@ def list_chassis_per_season():
                     for link in teams_links:
                         teams_season_list.append(link.get('title'))
                 dict_chassis_season[season]=teams_season_list
-    with open('chassis_season.txt', 'w') as file:
-        file.write(str(dict_chassis_season))
+    json_dict = json.dumps(dict_chassis_season)
+    with open(json_file, 'w') as file:
+        file.write(json_dict)
+
+# This function creates folders to store the photos of each car
+def create_folders():
+    folder = 'photos'
+    with open(json_file, 'r') as file:
+        data = file.read()
+    loaded_json = json.loads(data)
+    if not os.path.isdir(folder):
+            os.mkdir(folder)
+
+    for k,v in loaded_json.items():
+        if not os.path.isdir(os.path.join(folder,k)):
+            os.mkdir(os.path.join(folder, k))
+
+        for team in v:
+            team = re.sub(r"[^a-zA-Z0-9]+", ' ', team)
+            if not os.path.isdir(os.path.join(folder,k,k+' - '+team)):
+                os.mkdir(os.path.join(folder,k,k+' - '+team))
+
+
+def download_photos(amount):
+    with open(json_file, 'r') as file:
+        data = file.read()
+    loaded_json = json.loads(data)
+    for k in loaded_json.keys():
+        chassis_season = os.listdir(os.path.join('photos',k))
+
+        path = '/usr/local/bin/chromedriver'
+        WINDOW_SIZE = "1920,1080"
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
+        chrome_options.add_argument('--no-sandbox')
+        driver = webdriver.Chrome(executable_path=path)
+
+        for chassis in chassis_season:
+            url = f'https://www.google.com/search?tbm=isch&q={chassis}'
+            driver.get(url)
+            time.sleep(0.5)
+            download_list = []
+            download_list.clear()
+
+            for el in range(amount):
+                try:
+                    driver.find_element_by_xpath(f'/html/body/div[2]/c-wiz/div[3]/div[1]/div/div/div/div/div[1]/div[1]/div[{el+1}]/a[1]/div[1]/img').click()
+                    time.sleep(0.5)
+                    img = driver.find_element_by_xpath('/html/body/div[2]/c-wiz/div[3]/div[2]/div[3]/div/div/div[3]/div[2]/c-wiz/div[1]/div[1]/div/div[2]/a/img').get_property(
+                        'src')
+                    if img[-3:].lower() in ['jpg','png']:
+                        download_list.append(img)
+                except:
+                    pass
+
+            if len(download_list) == 0:
+                current_folder = os.path.join('photos',k,chassis)
+                current_photos = os.listdir(current_folder)
+                for id, photo in enumerate(current_photos):
+                    old=photo
+                    new=str(id+100)+photo[-4:]
+                    try:
+                        os.rename(os.path.join(current_folder,old),os.path.join(current_folder,new))
+                    except:
+                        pass
+
+                    last_id = id
+
+                for item, link in enumerate(download_list):
+                    path = os.path.join('photos',k,chassis,str(last_id+item+101)+link[-4:])
+
+                    try:
+                        ulib.urlretrieve(link, path)
+                        print(f'Arquivo {path} salvo!')
+                    except:
+                        print('Failed ULIB')
+
 
 
 # Checks wether is necessary to run 'list_chassis_per_year' function
-if not os.path.isfile('chassis_season.txt'):
+if not os.path.isfile(json_file):
     list_chassis_per_season()
-
+create_folders()
+download_photos(100)
